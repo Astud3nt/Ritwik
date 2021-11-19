5b158ecf421a7f75f6a951e3731e88dade99290b
Astud3nt <94380943+Astud3nt@users.noreply.github.com>
2021-11-19 17:10:17 +0530

    Update char_pointers.c

--- CS/char_pointers.c
+++ CS/char_pointers.c
@@ -4,7 +4,7 @@ int main(void)
 	int x;
 	char*st = "Kolkata";
 	char str[10] = "Kolkata";
-	char*str_array[] = {"Messi", "Guardiola", "Cruyff", "Iniesta", "Xavi"};
+	char*str_array[] = {"India", "Pakistan", "Bhutan", "Nepal", "China"};
 	for(x=0; x<5; x++)
 		printf("%s \n", str_array[x]);
 	printf("%p \n", str);
